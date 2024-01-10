1d9fd2b19ce7f539ebfc01015b2036fdc140fe69
komachihashimoto <joujinameko@gmail.com>
2024-01-10 13:41:05 +0900

    first

--- janken.go
+++ janken.go
@@ -4,5 +4,26 @@ import "fmt"
 
 func main() {
 
-	
+	pattern := [3]string{"グー","チョキ","パー"}
+
+	fmt.Println("****じゃんけん勝敗リスト****")
+
+	for me := 0; me < 3; me++ {
+
+		fmt.Printf("私が%sのとき:\n", pattern[me])
+
+		for you := 0; you < 3; you++ {
+
+			score := (3 + me - you) % 3
+
+			if score == 2 {
+				fmt.Printf("あなたが%sなら私の勝ち\n", pattern[you])
+			} else if score == 1 {
+				fmt.Printf("あなたが%sなら私の負け\n", pattern[you])
+			} else {
+				fmt.Printf("あなたが%sならあいこ\n", pattern[you])
+			}
+		}
+		fmt.Println()
+	}
 }
